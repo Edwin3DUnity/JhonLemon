68cb27df074c5d677e64fc600292019b00a408b0
unknown <eapaniagua8@misena.edu.co>
2024-07-08 11:37:55 -0500

    Sistema de waypoints

--- Assets/_Scripts/WaypointPatrol.cs
+++ Assets/_Scripts/WaypointPatrol.cs
@@ -10,19 +10,26 @@ public class WaypointPatrol : MonoBehaviour
     private NavMeshAgent navMeshAgent;
 
     public Transform[] wayPoints;
-    
+
+    private int currentWaypointIndex;
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
 
-        navMeshAgent.SetDestination(wayPoints[0].position);
+        navMeshAgent.SetDestination(wayPoints[currentWaypointIndex].position);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % wayPoints.Length;
+            navMeshAgent.SetDestination(wayPoints[currentWaypointIndex].position);
+
+        }
         
     }
 }
